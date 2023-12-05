e5fa0a696fd6b26ef1518ca3b5dfb71005677ab1
LandonHM <pureskillezsandwitch@gmail.com>
2023-12-05 16:13:24 -0600

    Added multithreading to make the brute force go faster, it took 1 hour

--- 2023/day5/part2.rs
+++ 2023/day5/part2.rs
@@ -1,13 +1,14 @@
 use std::fs::read_to_string;
+use std::thread;
 
 #[allow(unused_variables)]
 fn main() {
     let mut min = 1000000000;
-    let file_in = read_to_string("sample_input.txt").unwrap();
-    //let file_in = read_to_string("input.txt").unwrap();
+    //let file_in = read_to_string("sample_input.txt").unwrap();
+    let file_in = read_to_string("input.txt").unwrap();
     let mut iter = file_in.lines().into_iter();
     let seeds: Vec<usize> = iter.next().expect("first line should exist").trim_start_matches("seeds: ").split(" ").map(|x| x.parse::<usize>().expect("should be number")).collect();
-    let mut maps: Vec<Vec<Vec<usize>>> = vec![];
+    let mut maps: Vec<Vec<Vec<isize>>> = vec![];
     let mut i = 0;
     // Put maps into vectors
     for line in iter {
@@ -15,7 +16,7 @@ fn main() {
             continue;
         }
         if line.as_bytes()[0].is_ascii_digit() {
-            maps[i-1].push(line.split(" ").map(|x| x.parse::<usize>().expect("Should be numbers in maps")).collect::<Vec<usize>>());
+            maps[i-1].push(line.split(" ").map(|x| x.parse::<isize>().expect("Should be numbers in maps")).collect::<Vec<isize>>());
         } else {
             i += 1;
             maps.push(vec![]);
@@ -28,39 +29,47 @@ fn main() {
     }
 
     // Find each seed and get lowest
-    let mut current;
-    let mut idx;
+    let mut threads: Vec<_> = vec![];
     let seed = seeds[0];
-    for chunk in seeds.chunks_exact(2) {
-        println!(" at {chunk:?}");
-        for i in chunk[0]..chunk[0]+chunk[1] {
-            current = i;
-            for map in maps.iter() {
-                idx = 0;
-                loop {
-                    // if is in the range, then remap
-                    let mapping = map.get(idx);
-                    match mapping {
-                        Some(m) => {
-                            if current >= m[1] && current < m[1] + m[2] {
-                                if m[1] < m[0]{
+    for (i,chunk) in seeds.chunks_exact(2).enumerate() {
+        let chunk = chunk.clone();
+        let maps = maps.clone();
+        let start = chunk[0];
+        let range = chunk[1];
+        println!("starting thread {i} with s = {start} r = {range}");
+        threads.push(thread::spawn(move || {
+            let mut min = 1000000000;
+            for i in start..start+range {
+                let mut current = i as isize;
+                for map in maps.iter() {
+                    let mut idx = 0;
+                    loop {
+                        // if is in the range, then remap
+                        let mapping = map.get(idx);
+                        match mapping {
+                            Some(m) => {
+                                if current >= m[1] && current < m[1] + m[2] {
                                     current += m[0] - m[1];
+                                    break;
                                 }
-                                else {
-                                    current -= m[1] - m[0];
-                                }
+                            },
+                            None => {
                                 break;
-                            }
-                        },
-                        None => {
-                            break;
-                        },
+                            },
+                        }
+                        idx += 1;
                     }
-                    idx += 1;
                 }
+                min = std::cmp::min(min,current);
             }
-            min = std::cmp::min(min,current);
-        }
+            return min;
+        }));
+    }
+    let mut i = 0;
+    for t in threads {
+        min = std::cmp::min(t.join().unwrap(),min);
+        println!("thread {i} finished");
+        i += 1;
     }
     //dbg!(maps);
     println!("{min}");
